80635224022ba5a02680ffbf1caf0a78d7a48734
Michael Titov <michaeltitov1@gmail.com>
2021-12-30 14:05:03 +0200

    upd SmapleScene
    upd Sample Scene quit button

--- Programming Theory Project/Assets/Scripts/TitleScreenManager.cs
+++ Programming Theory Project/Assets/Scripts/TitleScreenManager.cs
@@ -3,11 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class TitleScreenManager : MonoBehaviour
 {
     public void StartSampleScene()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
     }
 }
